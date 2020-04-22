e11a8e777450e8f62768ccb5948f58e8fe439ac7
liuziqiao <mtxqliu@163.com>
2020-04-22 21:41:16 +0800

    初建工程

---
+++ liu-gateway/src/main/java/org/liu/liugateway/LiuGatewayApplication.java
@@ -0,0 +1,13 @@
+package org.liu.liugateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiuGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiuGatewayApplication.class, args);
+    }
+
+}
---
+++ liu-gateway/src/test/java/org/liu/liugateway/LiuGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.liu.liugateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiuGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
